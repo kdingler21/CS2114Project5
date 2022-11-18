5c0a18d880cb7a2d0e36141acfe4357f435a1007
Sushen Kolakaleti - Yamuna Software Solutions <yamunasoftware18@gmail.com>
2022-11-18 17:37:48 -0500

    Add files via upload

--- src/project5/Input.java
+++ src/project5/Input.java
@@ -1,5 +1,13 @@
 package project5;
 
 public class Input {
-
+    /**
+     * Main Run Method
+     */
+    public static void main(String[] args) {
+        String inputFile = "SampleInput1_2022.csv";
+        if (args.length > 0) {
+            inputFile = args[0];
+        }
+    }
 }
