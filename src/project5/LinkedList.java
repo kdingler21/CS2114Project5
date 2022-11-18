3bad069f6dd60c6f0436ef416e7f340b3cd5e629
Sushen Kolakaleti - Yamuna Software Solutions <yamunasoftware18@gmail.com>
2022-11-18 17:57:00 -0500

    Add files via upload

--- src/project5/LinkedList.java
+++ src/project5/LinkedList.java
@@ -13,4 +13,42 @@ public class LinkedList {
         this.firstNode = first;
         this.size++;
     }
+    
+    public void add(Influencer added) {
+        Influencer current = firstNode;
+        int count = 0;
+        while (current != null && count < size) {
+            if (current.getNext() == null && count == size-1) {
+                current.setNext(added);
+            }
+            
+            current = current.getNext();
+            count++;
+        }
+    }
+    
+    public Influencer remove() {
+        Influencer current = firstNode;
+        Influencer node = null;
+        mainLoop: while (current != null) {
+            if (current.getNext() != null) {
+                if (current.getNext().getNext() == null) {
+                    node = current.getNext();
+                    current.setNext(null);
+                    break mainLoop;
+                }
+            }
+            
+            current = current.getNext();
+        }
+        return node;
+    }
+    
+    public String toString() {
+        String output = "";
+        for (int i = 0; i < size; i++) {
+            
+        }
+        return output;
+    }
 }
