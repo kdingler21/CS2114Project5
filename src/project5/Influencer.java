d24ffd27901af8465fe84aaf976f89d14d300df7
Catherine Dingler <katiedingler@Catherines-MacBook-Pro.local>
2022-11-18 19:37:13 -0500

    im silly

--- src/project5/Influencer.java
+++ src/project5/Influencer.java
@@ -1,7 +1,18 @@
 package project5;
 
-public class Influencer {
+public class Influencer 
+{
     private Influencer nextNode;
+    private String month;
+    private String username;
+    private String channelName;
+    private String country;
+    private String topic;
+    private int likes;
+    private int posts;
+    private int followers;
+    private int comments;
+    private int views;
     
     public Influencer() {
         this.nextNode = null;
@@ -18,4 +29,87 @@ public class Influencer {
     public Influencer getNext() {
         return nextNode;
     }
+        
+        /**
+         * sets up each individual influencer 
+         * @param mon month 
+         * @param user username 
+         * @param chanName channel Name 
+         * @param count country location 
+         * @param top topic of user 
+         * @param lik likes 
+         * @param post amount of posts
+         * @param follow follower count
+         * @param comm comment count
+         * @param view views received 
+         */
+        public Influencer(String mon, String user, String chanName, 
+            String count, String top, int lik, int post, int follow, int comm, int view)
+        {
+            this.month = mon;
+            this.username = user;
+            this.channelName = chanName;
+            this.country = count;
+            this.topic = top;
+            this.likes = lik;
+            this.posts = post;
+            this.followers = follow;
+            this.comments = comm;
+            this.views = view;
+        }
+        
+        /**
+         * 
+         * @return
+         */
+        public String getMonth()
+        {
+            return month;
+        }
+        
+        public String getUserName()
+        {
+            return username;
+        }
+        
+        public String getChannelName()
+        {
+            return channelName;
+        }
+        
+        public String getCountry()
+        {
+            return country;
+        }
+        
+        public String getTopic()
+        {
+            return topic;
+        }
+        
+        public int getLikes()
+        {
+            return likes;
+        }
+        
+        public int getPosts()
+        {
+            return posts;
+        }
+        
+        public int getFollowers()
+        {
+            return followers;
+        }
+        
+        public int getComments()
+        {
+            return comments;
+        }
+        
+        public int getViews()
+        {
+            return views;
+        }
+        
 }
