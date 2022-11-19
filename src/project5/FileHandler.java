c9f58560113d761dc519adcff26c6ddeaefc96b2
Sushen Kolakaleti - Yamuna Software Solutions <yamunasoftware18@gmail.com>
2022-11-18 23:39:24 -0500

    Add files via upload

--- src/project5/FileHandler.java
+++ src/project5/FileHandler.java
@@ -17,7 +17,20 @@ public class FileHandler {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             while ((line = br.readLine()) != null) {  
                 String data[] = line.split(splitBy);
-                list.add(new Influencer());
+                
+                String month = data[0];
+                String username = data[1];
+                String channelName = data[2];
+                String country = data[3];
+                String topic = data[4];
+                int likes = Integer.parseInt(data[5]);
+                int posts = Integer.parseInt(data[6]);
+                int followers = Integer.parseInt(data[7]);
+                int comments = Integer.parseInt(data[8]);
+                int views = Integer.parseInt(data[9]);
+                
+                list.add(new Influencer(month, username, channelName, country, topic,
+                    likes, posts, followers, comments, views));
             }   
         }
         
