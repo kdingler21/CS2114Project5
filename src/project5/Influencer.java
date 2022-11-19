6a25b476b551b3d7c1ef28fd1ec620cdc84a4ec7
mikeferrante123 <118482545+mikeferrante123@users.noreply.github.com>
2022-11-19 14:52:31 -0500

    Update Influencer.java

--- src/project5/Influencer.java
+++ src/project5/Influencer.java
@@ -1,5 +1,11 @@
 package project5;
 
+/**
+ * 
+ * @author Mike Ferrante, Katie Dingler, Sushen Kolakaketi
+ * @version 11.19.2022
+ * 
+ */
 public class Influencer {
     private Influencer nextNode;
     private String month;
@@ -13,31 +19,78 @@ public class Influencer {
     private int comments;
     private int views;
 
+    /**
+     * constructor, sets next node to null
+     */
     public Influencer() {
         this.nextNode = null;
     }
 
 
+    /**
+     * constructor sets next node to next influencer
+     * 
+     * @param next
+     *            new influencer to be set next
+     */
     public Influencer(Influencer next) {
         this.nextNode = next;
     }
 
 
+    /**
+     * sets next node to new influencer
+     * 
+     * @param next
+     *            new influencer
+     */
     public void setNext(Influencer next) {
         this.nextNode = next;
     }
 
 
+    /**
+     * getter method, gets next node
+     * 
+     * @return nextNode
+     *         next node in LinkedList
+     * 
+     */
     public Influencer getNext() {
         return nextNode;
     }
-    
+
+
+    /**
+     * gets influencers total engagement by adding comments and likes
+     * 
+     * @return
+     *         the total engagement
+     */
     public double getTotalEngagement() {
         return (this.comments + this.likes);
     }
-    
-    public double getEngagementRate() {
+
+
+    /**
+     * gets engagement rate using traditional formula
+     * 
+     * @return
+     *         engagement rate
+     */
+    public double getTraditionalEngagementRate() {
         return ((this.getTotalEngagement() / this.followers) * 0.1);
+    }
+
+
+    /**
+     * gets engagement rate using reach formula
+     * 
+     * @return
+     *         engagement rate
+     */
+    public double getReachEngagementRate() {
+        return ((this.getTotalEngagement() / this.views) * 0.1);
     }
 
 
@@ -89,71 +142,165 @@ public class Influencer {
     }
 
 
+    /**
+     * getter method, gets month
+     * 
+     * @return
+     *         month
+     */
     public String getMonth() {
         return month;
     }
 
 
+    /**
+     * getter method, gets user name
+     * 
+     * @return
+     *         username
+     */
     public String getUserName() {
         return username;
     }
 
 
+    /**
+     * getter method, gets channel name
+     * 
+     * @return
+     *         channelName
+     */
     public String getChannelName() {
         return channelName;
     }
 
 
+    /**
+     * getter method, gets country
+     * 
+     * @return
+     *         country
+     */
     public String getCountry() {
         return country;
     }
 
 
+    /**
+     * getter method, gets topic
+     * 
+     * @return
+     *         topic
+     */
     public String getTopic() {
         return topic;
     }
 
 
+    /**
+     * getter method, gets likes
+     * 
+     * @return
+     *         likes
+     */
     public int getLikes() {
         return likes;
     }
 
 
+    /**
+     * getter method, gets posts
+     * 
+     * @return
+     *         posts
+     */
     public int getPosts() {
         return posts;
     }
 
 
+    /**
+     * getter method, gets followers
+     * 
+     * @return
+     *         followers
+     */
     public int getFollowers() {
         return followers;
     }
 
 
+    /**
+     * getter method, gets comments
+     * 
+     * @return
+     *         comments
+     */
     public int getComments() {
         return comments;
     }
 
 
+    /**
+     * getter method, gets views
+     * 
+     * @return
+     */
     public int getViews() {
         return views;
     }
-    
+
+
+    /**
+     * sets likes
+     * 
+     * @param likes
+     *            new value
+     */
     public void setLikes(int likes) {
         this.likes = likes;
     }
-    
+
+
+    /**
+     * sets posts
+     * 
+     * @param posts
+     *            new value
+     */
     public void setPosts(int posts) {
         this.posts = posts;
     }
-    
+
+
+    /**
+     * sets followers
+     * 
+     * @param followers
+     *            new value
+     */
     public void setFollowers(int followers) {
         this.followers = followers;
     }
-    
+
+
+    /**
+     * sets comments
+     * 
+     * @param comments
+     *            new value
+     */
     public void setComments(int comments) {
         this.comments = comments;
     }
-    
+
+
+    /**
+     * sets views
+     * 
+     * @param views
+     *            new value
+     */
     public void setViews(int views) {
         this.views = views;
     }
