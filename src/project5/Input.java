0f89b3d7af5ebb025906ad60cafa397eab76a05b
katiedingler <katiedingler@catherines-mbp.home>
2022-11-20 17:50:58 -0500

    "Project update"

--- src/project5/Input.java
+++ src/project5/Input.java
@@ -10,7 +10,7 @@ public class Input {
      * Main Run Method
      */
     public static void main(String[] args) {
-        String inputFile = "SampleInput1_2022.csv";
+        String inputFile = "SampleInput1_2022.csv"; 
         if (args.length > 0) {
             inputFile = args[0];
         }
--- src/project5/LinkedListTest.java
+++ src/project5/LinkedListTest.java
@@ -1,20 +1,60 @@
 package project5;
-
+import student.TestCase;
 /**
  * testing the linked list 
  * @author katiedingler
  * @version
  */
-public class LinkedListTest 
+public class LinkedListTest extends TestCase
 {
     private LinkedList listDefault;
     private LinkedList list;
+    private Influencer influencer;
+    private Influencer influencer2;
+    private Influencer influencer3;
+    private Influencer influencer4;
     
     /**
      * sets up the test method
      */
     public void setUp()
     {
+        influencer = new Influencer("jan", "john", "john123", "US", "sports", 10, 10, 10, 10, 20); 
+        influencer2 = new Influencer("feb", "mike", "mike1", "US", "sports", 5, 5, 5, 5, 4); 
+        influencer3 = new Influencer("mar", "kev", "john123", "US", "sports", 20, 20, 20, 20, 5); 
+        influencer4 = new Influencer("aug", "kyle", "mike1", "US", "sports", 15, 15, 15, 15, 14);
         
+        listDefault = new LinkedList();
+        listDefault.add(influencer);
+        listDefault.add(influencer2);
+        listDefault.add(influencer3);
+        
+        list = new LinkedList(influencer);
+        list.add(influencer2);
+        list.add(influencer3);        
+    }
+    /**
+     * getting the first node
+     */
+    public void testGetFirstNode()
+    {
+        assertEquals(listDefault.getFirstNode(), influencer);
+        assertEquals(list.getFirstNode(), influencer);
+    }
+    /** 
+     * testing get and set the second 
+     */
+    public void testNext()
+    {
+        assertEquals(list.getFirstNode(), influencer);
+        listDefault.setNext(influencer2);
+        assertEquals(listDefault.next(), influencer2);
+    }
+    /**
+     * get size 
+     */
+    public void testGetSize()
+    {
+        assertEquals(listDefault.getSize(), 3);
     }
 }
