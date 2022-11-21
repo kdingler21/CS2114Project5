3711ad4f774dd66f13672ad86c2e2585f75a824d
mikeferrante123 <118482545+mikeferrante123@users.noreply.github.com>
2022-11-21 16:31:22 -0500

    Update Input.java

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -4,50 +4,50 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 
 /**
- * Input class, project runner 
+ * Input class, project runner
+ * 
  * @author Mike Ferrante, Katie Dingler, Sushen Kolakaketi
  * @version 11/19/22
  */
 public class Input {
     /**
      * Main Run Method
-     * @throws ParseException 
-     * @throws FileNotFoundException 
+     * 
+     * @throws FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException, ParseException { 
-        String inputFile = "SampleInput1_2022.csv"; 
+    public static void main(String[] args)
+        throws FileNotFoundException {
+        String inputFile = "SampleInput1_2022.csv";
         if (args.length > 0) {
             inputFile = args[0];
         }
-        
-        FileHandler handler = new FileHandler(); 
-        Sorter sorter = new Sorter(); 
-        
+
+        FileHandler handler = new FileHandler();
+        Sorter sorter = new Sorter();
+
         LinkedList list = handler.readFile(inputFile);
         LinkedList sorted = sorter.sortChannels(list);
-        
-        Influencer[] sortedAlphabet = sorter.sortByChannelName(sorted);
-        Influencer[] sortedEngagement = sorter.sortByReachEngagementRate(sorted);
-        
 
-        for (int i = 0; i < sortedAlphabet.length; i++)
-        {
-            if (sortedAlphabet[i] != null)
-            {
+        Influencer[] sortedAlphabet = sorter.sortByChannelName(sorted);
+        Influencer[] sortedEngagement = sorter.sortByReachEngagementRate(
+            sorted);
+
+        for (int i = 0; i < sortedAlphabet.length; i++) {
+            if (sortedAlphabet[i] != null) {
                 System.out.println(sortedAlphabet[i].getChannelName());
-                System.out.println(sortedAlphabet[i].getTraditionalEngagementRate());
-                System.out.println("==========");               
-            } 
+                System.out.println(sortedAlphabet[i]
+                    .getTraditionalEngagementRate());
+                System.out.println("==========");
+            }
         }
         System.out.println("**********");
         System.out.println("**********");
-        for (int i = 0; i < sortedEngagement.length; i++)
-        {
-            if (sortedEngagement[i] != null)
-            {
+        for (int i = 0; i < sortedEngagement.length; i++) {
+            if (sortedEngagement[i] != null) {
                 System.out.println(sortedEngagement[i].getChannelName());
-                System.out.println(sortedEngagement[i].getTraditionalEngagementRate());
-                System.out.println("==========");               
+                System.out.println(sortedEngagement[i]
+                    .getTraditionalEngagementRate());
+                System.out.println("==========");
             }
         }
     }
