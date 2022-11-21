babbb922184b1f043c910986ffe15e31c9bb9b08
mikeferrante123 <118482545+mikeferrante123@users.noreply.github.com>
2022-11-21 16:29:50 -0500

    Update InfluencerTest.java

--- src/prj5/InfluencerTest.java
+++ src/prj5/InfluencerTest.java
@@ -1,28 +1,32 @@
 package prj5;
+
 import student.TestCase;
 
 /**
- * test class for the influencer 
+ * test class for the influencer
+ * 
  * @author katiedingler
  * @version 11/19/22
  */
-public class InfluencerTest extends TestCase
-{
+public class InfluencerTest extends TestCase {
     private Influencer influencer;
     private Influencer influencer2;
+
     /**
      * sets up test case
      */
-    public void setUp()
-    {
-        influencer = new Influencer("jan", "john", "john123", "US", "sports", 10, 10, 10, 10, 20);
-        influencer2 = new Influencer("feb", "mike", "mike1", "US", "sports", 5, 5, 5, 5, 4);
+    public void setUp() {
+        influencer = new Influencer("jan", "john", "john123", "US", "sports",
+            10, 10, 10, 10, 20);
+        influencer2 = new Influencer("feb", "mike", "mike1", "US", "sports", 5,
+            5, 5, 5, 4);
     }
+
+
     /**
-     * tests all the getters 
+     * tests all the getters
      */
-    public void testGetters()
-    {
+    public void testGetters() {
         assertTrue(influencer.getUserName().equals("john"));
         assertTrue(influencer.getMonth().equals("jan"));
         assertTrue(influencer.getChannelName().equals("john123"));
@@ -34,11 +38,12 @@ public class InfluencerTest extends TestCase
         assertEquals(10, influencer.getComments());
         assertEquals(20, influencer.getViews());
     }
+
+
     /**
-     * checks engagement rates 
+     * checks engagement rates
      */
-    public void testGetEngagement()
-    {
+    public void testGetEngagement() {
         assertEquals(influencer.getTotalEngagement(), 20, .01);
         assertEquals(influencer.getTraditionalEngagementRate(), .2, .01);
         assertEquals(influencer.getReachEngagementRate(), .1, .01);
